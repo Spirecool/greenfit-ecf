663a8d46099fe879af4fe5211cf29b930db3b3b9
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2022-08-27 16:38:16 +0200

    MODS du fichier security.yaml: routes login et logout

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class AdminController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_admin')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
         ]);
     }
 }
